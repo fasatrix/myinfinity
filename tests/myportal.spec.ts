e3b06a2319611d8011c93ed587e2a0a7138ba516
Fabio Santoro <fasatrix@gmail.com>
2025-01-22 15:14:49 +0100

    first commit

---
+++ tests/myportal.spec.ts
@@ -0,0 +1,10 @@
+import { test } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('');
+  await page.getByPlaceholder('Username').fill(process.env.USERNAME ?? "");
+  await page.getByPlaceholder('Password').fill(process.env.PASSWORD ?? "");
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.pause()
+  await page.locator('iframe[name="Main"]').contentFrame().getByRole('button', { name: 'Presenza' }).click();
+});
