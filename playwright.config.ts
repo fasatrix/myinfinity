fe364f4e26a351776866b1f580b9bc07def12d10
Fabio Santoro <fasatrix@gmail.com>
2025-01-23 16:02:15 +0100

    change conf and increase timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,22 +7,29 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+
+  reporter: [['html'], ['list']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    /* Fail the build on CI if you accidentally left test.only in the source code. */
   use: {
-    baseURL: process.env.URL,
-    trace: 'on-first-retry',
+    baseURL: process.env.URL
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { 
+        ...devices['Desktop Chrome'],
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure',
+        trace: 'on-first-retry',
+      },
+      
     },
   ],
 });
